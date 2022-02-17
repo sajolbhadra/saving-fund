53119a152df3b0fa1b1b0588c36644e6b9a2133e
sajolbhadra <sajolbhadra@gmail.com>
2022-02-17 19:55:47 +0600

    basic ui created

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// --------------Calculate Button Handler----------------
+const food= document.getElementById('food-input');
+const rent= document.getElementById('rent-input');
+const food= document.getElementById('cloths-input');
